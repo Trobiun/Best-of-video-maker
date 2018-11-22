dffd9aceab19b4ab48d3ead70b3c93c676ddcfa5
Trobiun <robin.tissot@free.fr>
2018-11-22 17:47:14 +0100

    la durée de l'extrait est calculée avec ffprobe

--- extractCuts.sh
+++ extractCuts.sh
@@ -15,14 +15,16 @@ extract() {
 	then
 		args="${FFMPEG_TEMP_CODEC_OPTIONS} ${FFMPEG_TEMP_ARGS}"
 		ss=""
-		if [ "${start}" != "00:00:00" ]
+		tot=""
+		if [ "${start}" != "00:00:00" -a "${start}" != "00:00:00.000" ]
 		then
 			ss="-ss ${start}"
-			args="-t ${duration} ${args}"
+			tot="-t ${duration}"
 		else
-			args="-to ${end} ${args}"
+			tot="-to ${end}"
 		fi
-		"${ffmpeg_bin}" ${ss} -i "${input}" ${args} "${output}" < /dev/null
+		args="${tot} ${args}"
+		"${ffmpeg_bin}" -loglevel ${FFMPEG_LOG_LEVEL} ${ss} -i "${input}" ${args} "${output}" < /dev/null
 	fi
 }
 
@@ -31,28 +33,30 @@ while IFS= read -r 'video'
 do
 	filebasename="$(basename "${video}")"
 	filename="${filebasename%.*}"
-	dir="${project_name}/${TEMP_DIR_NAME}/${filename}"
+	dir="${PROJECTS_DIR}/${project_name}/${TEMP_DIR_NAME}/${CUTS_DIR_NAME}/${filename}"
 	mkdir -p "${dir}"
 	while IFS= read -r 'list_file'
 	do
 		type_extracts="${list_file##*.}"
 		type_extracts_dir="${dir}/${type_extracts}"
-		mkdir -p "${type_extracts_dir}/${CUTS_DIR_NAME}"
+		mkdir -p "${type_extracts_dir}" #/${CUTS_DIR_NAME}
 		while IFS= read -r 'line' || [[ -n "${line}" ]]
 		do
 			number=$(cut -f1 <<< "${line}")
 			start=$(cut -f2 <<< "${line}")
 			end=$(cut -f3 <<< "${line}")
-			duration=$(./diffTime.sh "${start}" "${end}")
+			#duration=$(./diffTime.sh "${start}" "${end}")
 			filename_escaped=$(sed -e 's/-/_/' <<< "${filename}")
 			cut_filename="${filename_escaped} - ${type_extracts} - ${number}"
 			cut_file="${cut_filename}.${FFMPEG_TEMP_EXT}"
-			extract "${video}" "${start}" "${end}" "${type_extracts_dir}/${CUTS_DIR_NAME}/${cut_file}"
-			final_clips_lists="${project_name}/${INPUT_DIR_NAME}/${CLIPS_LIST_DIR_NAME}/${cut_filename}.clips"
+			extract "${video}" "${start}" "${end}" "${type_extracts_dir}/${cut_file}" #/${CUTS_DIR_NAME}
+			final_clips_lists="${PROJECTS_DIR}/${project_name}/${INPUT_DIR_NAME}/${CLIPS_LIST_DIR_NAME}/${cut_filename}.clips"
 			if [ ! -f "${final_clips_lists}" ]
 			then
-				echo "01	00:00:00.000	${duration}.000" > "${final_clips_lists}"
+				duration=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal "${type_extracts_dir}/${cut_file}")
+				mkdir -p "${PROJECTS_DIR}/${project_name}/${INPUT_DIR_NAME}/${CLIPS_LIST_DIR_NAME}"
+				echo "01	00:00:00.000	${duration}" > "${final_clips_lists}"
 			fi
 		done < "${list_file}"
-	done < <(find "${project_name}" -type f -name "${filename}.*" -not -name "${filebasename}" -print)
-done < <(find "${project_name}/${INPUT_DIR_NAME}" -maxdepth 1 -type f -exec file -N --mime-type -- {} + | sed -n 's!: video/[^:]*$!!p')
+	done < <(find "${PROJECTS_DIR}/${project_name}" -type f -name "${filename}.*" -not -name "${filebasename}" -print)
+done < <(find "${PROJECTS_DIR}/${project_name}/${INPUT_DIR_NAME}" -maxdepth 1 -type f -exec file -N --mime-type -- {} + | sed -n 's!: video/[^:]*$!!p')
