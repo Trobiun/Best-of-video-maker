a845019ae25dd2f276c7a351c315e18517f2e46e
Trobiun <robin.tissot@free.fr>
2018-11-22 17:57:49 +0100

    gestion d'un dossier global pour les projets

--- insertTransition.sh
+++ insertTransition.sh
@@ -3,9 +3,9 @@ set -o errexit
 set -o nounset
 
 
-transition_video="${project_name}/${INPUT_DIR_NAME}/${INPUT_DIR_TRANSITION}/${TRANSITION_VIDEO_NAME}"
+transition_video="${PROJECTS_DIR}/${project_name}/${INPUT_DIR_NAME}/${INPUT_DIR_TRANSITION}/${TRANSITION_VIDEO_NAME}"
 if [ -s "${transition_video}" ]
 then
-	final_list_clips_file="${project_name}/${INPUT_DIR_NAME}/${FINAL_CLIPS_LIST_FILE}"
+	final_list_clips_file="${PROJECTS_DIR}/${project_name}/${INPUT_DIR_NAME}/${FINAL_CLIPS_LIST_FILE}"
 	awk -i inplace -v line="file '../../${transition_video}'" '/^.*$/{ printf "%s\n%s\n", $0, line; next;}; 1' "${final_list_clips_file}"
 fi
